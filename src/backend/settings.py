544b8a3bb62d8ab4995e2173fa4dd5403b3a0f6c
Hector Pulido <hectorandrespp@gmail.com>
2021-09-08 01:49:11 -0500

    docker file, and heroku deploy

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", default="foo")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=1))
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
